3b8c62da83f7852a6a8ad7373579b5959f408d5d
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-09 13:07:53 +1000

    adding light ability

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -1,17 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Player2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AnimationCurve curve;
+
+    public float cooldown;
+    private float coolDownElapsed = -99f;
+    
+    [Header("Light component")]
+    private float startInner = 0f;
+    private float startOuter = 5f;
+    private float startIntensity = 2f;
+
+    private float endInner = 5f;
+    private float endOuter = 10f;
+    private float endIntensity = 5f;
+    
+    public float activateDuration;
+
+    private Light2D light;
+
+    private void Awake()
     {
-        
+        light = GetComponentInChildren<Light2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        Move();
+        Activate();
+    }
+
+    void Move()
     {
         transform.position = GetMouseWorldPosition(Input.mousePosition);
     }
@@ -19,5 +43,40 @@ public class Player2 : MonoBehaviour
     Vector2 GetMouseWorldPosition(Vector2 mousePos)
     {
         return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    void Activate()
+    {
+        if (coolDownElapsed + cooldown > Time.time) return;
+        
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            coolDownElapsed = Time.time;
+            StartCoroutine(ToggleLightCoroutine());
+        }
+    }
+
+    IEnumerator ToggleLightCoroutine()
+    {
+        float startTime = Time.time;
+        while (startTime + activateDuration >= Time.time)
+        {   
+            light.intensity = Mathf.Lerp(startIntensity, endIntensity, curve.Evaluate(Time.time - startTime / activateDuration));
+            light.pointLightInnerRadius = Mathf.Lerp(startInner, endInner, curve.Evaluate(Time.time - startTime / activateDuration));
+            light.pointLightOuterRadius = Mathf.Lerp(startOuter, endOuter, curve.Evaluate(Time.time - startTime / activateDuration));
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        
+        startTime = Time.time;
+        while (startTime + activateDuration >= Time.time)
+        {   
+            light.intensity = Mathf.Lerp(endIntensity, startIntensity, curve.Evaluate(Time.time - startTime / activateDuration));
+            light.pointLightInnerRadius = Mathf.Lerp(endInner, startInner, curve.Evaluate(Time.time - startTime / activateDuration));
+            light.pointLightOuterRadius = Mathf.Lerp(endOuter, startOuter, curve.Evaluate(Time.time - startTime / activateDuration));
+            yield return null;
+        }
     }
 }

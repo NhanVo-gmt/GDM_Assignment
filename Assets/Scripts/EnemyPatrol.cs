599a4d0c2b0f469fff614c6e667d347c10833592
Aidan <aidan.watkins@gmail.com>
2024-04-14 17:14:42 +1000

    Finished the first map. Need to add a win area but all enemies are on the map and will chase the player and when the player loses 3 lives the scene is reloaded

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 3f;
+    public float detectionRadius = 5f;
+
+    private Transform playerTransform;
+    private int waypointIndex = 0;
+    private float dist;
+    private Vector3 currentTarget;
+    private bool isChasing = false;
+
+    void Start()
+    {
+        currentTarget = waypoints[waypointIndex].position;
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+        if (playerTransform == null ) 
+        {
+            Debug.LogError("Player not found");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+
+            if (distanceToPlayer < detectionRadius)
+            {
+                isChasing = true;
+            }
+            else
+            {
+                isChasing = false;
+            }
+
+            if (isChasing)
+            {
+                ChasePlayer();
+            }
+            else
+            {
+                Patrol();
+            }
+        }
+    }
+
+    void Patrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget, patrolSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, currentTarget) < 0.1f)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+            currentTarget = waypoints[waypointIndex].position;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        currentTarget = playerTransform.position;
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget, chaseSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1);
+            }
+            else
+            {
+                Debug.LogError("PlayerHealth component not found");
+            }
+        }
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0 )
+        {
+            Debug.Log("Player has died!");
+            ReloadCurrentScene();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    void ReloadCurrentScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}

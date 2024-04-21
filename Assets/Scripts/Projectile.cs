caf05aa113925a2d5e7cab849f96382cec2c3878
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-21 23:27:23 +1000

    adding arrow trap

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float speed = 0;
+    private Vector2 direction;
+
+    private void Awake()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    public void Initialize(float speed, Vector2 direction)
+    {
+        transform.up = this.direction;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    private void Update()
+    {
+        if (speed == 0) return;
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

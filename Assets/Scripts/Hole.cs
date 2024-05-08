1b274eff63d78c1353c6bca355e6d655f9a79a86
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-05-08 19:20:38 +1000

    adding level 3

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1);
+            }
+            else
+            {
+                Debug.LogError("PlayerHealth component not found");
+            }
+        }
+    }
+}
